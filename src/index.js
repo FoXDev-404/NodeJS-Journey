a34eabda2bdb709578308c5667d55bae613485de
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-10-02 22:05:10 +0530

    Refactor server.js to return an array of user objects in JSON format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const http = require('http');
+
 http.createServer((_, res) => {
     res.writeHead(200, { 'content-type': 'application/json' });
-    res.write(JSON.stringify({ name: "@FoXDev-404", email: "foxdev_404@gmail.com" }));
+    res.write(JSON.stringify([
+        { name: "@FoXDev-404", email: "foxdev_404@gmail.com" },
+        { name: "Rajpal Nishad", email: "rajpal.nishad@gmail.com" },
+        { name: "Nikhil Pratap Singh", email: "nikhil.p.s@gmail.com" }
+    ]));
     res.end();
 }).listen(4040);
