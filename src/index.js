41e7b5cb148eed84ee203de9a405668394d99749
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-10-02 15:55:10 +0530

    Refactor server.js to remove HTML tags from response

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
-
-console.log('->>', __filename);
-fs.writeFileSync("filex.txt", "This is example of fs in js")
+const http = require('http');
+http.createServer((_, res) => {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.write(JSON.stringify({ name: "@FoXDev-404", email: "foxdev_404@gmail.com" }));
+    res.end();
+}).listen(4040);
